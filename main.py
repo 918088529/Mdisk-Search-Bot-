37a3be267df595ed4b76972488f957b6e588211c
SHOKDHAR <121552501+918088529@users.noreply.github.com>
2023-01-10 22:37:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import asyncio
 
 # Bot Client for Inline Search
 Bot = Client(
-    session_name=Config.BOT_SESSION_NAME,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN
+    session_name=Config.Mdisk_search_filter_bot,
+    api_id=Config.23098568,
+    api_hash=Config.cb7098aa919c29da3e5f9af0f9086dd7,
+    bot_token=Config.5809851448:AAEgTfUAhi8uytQjCSH2aJrpCSeciUMzkNg
 )
 
 # User Client for Searching in Channel.
 User = Client(
-    session_name=Config.USER_SESSION_STRING,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH
+    session_name=Config.1BVtsOL4Bu8UNyJ-TIOIvyDAJi4lzcDBjMmvc_xYC2MgZSYR1kNl2eFtrp4hgvAW4u0-RuDe4QbNMZDAX48EwZN__qqeTUBPeFAvN8XLcFvqBOJhzvNjezgsQm85BiKpW8dlOtruTLwLG_Hsctx21ZdjuCapMYyRJMx7dgms7VA2Da9Bm30M6gAliuZas1V2s09JybCci6Ml3qsv3wS4Up38zv8mEEuk1t_AbSy51KH4hBQKSZwi9JqbGsHeHa5crNKy8Ua7Aw1cluNAw3fICTQySkKctdd5fC9YzDOo84xuml1t0uRtm58GsBn1-yIJxuOMf6N9rnn9gcwiivHIVjXXqrrnMtYs=,
+    api_id=Config.23098568,
+    api_hash=Config.cb7098aa919c29da3e5f9af0f9086dd7
 )
 
 @Bot.on_message(filters.private & filters.command("start"))
@@ -26,8 +26,8 @@ async def start_handler(_, event: Message):
 	await event.reply_photo("https://telegra.ph/file/19eeb26fa2ce58765917a.jpg",
                                 caption=Config.START_MSG.format(event.from_user.mention),
                                 reply_markup=InlineKeyboardMarkup([
-					[InlineKeyboardButton('❤ Donation Link', url='https://www.telegram.dog/greymatters_about')],
-					[InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/GreyMatter_Bots")],
+					[InlineKeyboardButton('❤ Donation Link', url='https://t.me/ideal_bot_father')],
+					[InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/ideal_bot_father")],
 					[InlineKeyboardButton("Donation", callback_data="Help_msg"),
                                         InlineKeyboardButton("About", callback_data="About_msg")]
 				]))
@@ -37,8 +37,8 @@ async def help_handler(_, event: Message):
 
     await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
-		[InlineKeyboardButton('❤ Donation Link', url='https://www.telegram.dog/greymatters_about')
-	 ],[InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/GreyMatter_Bots"), 
+		[InlineKeyboardButton('❤ Donation Link', url='https://t.me/ideal_bot_father')
+	 ],[InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/ideal_bot_father"), 
              InlineKeyboardButton("𝙰𝚋𝚘𝚞𝚝", callback_data="About_msg")]
         ])
     )
@@ -47,8 +47,8 @@ async def help_handler(_, event: Message):
 async def inline_handlers(_, event: Message):
     if event.text == '/start':
         return
-    answers = f'**📂 Results For ➠ {event.text} \n\n➠ Type Only Movie Name With Correct Spelling.✍️\n➠ Add Year For Better Result.🗓️\n➠ Join @GreyMatter_Bots\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
-    async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
+    answers = f'**📂 Results For ➠ {event.text} \n\n➠ Type Only Movie Name With Correct Spelling.✍️\n➠ Add Year For Better Result.🗓️\n➠ Join @ideal_bot_father\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
+    async for message in User.search_messages(chat_id=Config.-1001844687848, limit=50, query=event.text):
         if message.text:
             thumb = None
             f_text = message.text
@@ -76,10 +76,10 @@ async def button(bot, cmd: CallbackQuery):
 			reply_markup=InlineKeyboardMarkup(
 				[
 					[
-						InlineKeyboardButton('❤ Donation Link', url='https://www.telegram.dog/greymatters_about')
+						InlineKeyboardButton('❤ Donation Link', url='https://t.me/ideal_bot_father')
 					],
 					[
-						InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/GreyMatter_Bots")
+						InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/ideal_bot_father")
 					],
 					[
 						InlineKeyboardButton("Home", callback_data="gohome")
@@ -95,10 +95,10 @@ async def button(bot, cmd: CallbackQuery):
 			reply_markup=InlineKeyboardMarkup(
 				[
 					[
-					InlineKeyboardButton('❤ Donation Link', url='https://www.telegram.dog/greymatters_about')
+					InlineKeyboardButton('❤ Donation Link', url='https://t.me/ideal_bot_father')
 					],
 					[
-					InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/GreyMatter_Bots")
+					InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/ideal_bot_father")
 					], 
                                         [
 					InlineKeyboardButton("Home", callback_data="gohome"),
@@ -115,10 +115,10 @@ async def button(bot, cmd: CallbackQuery):
 			reply_markup=InlineKeyboardMarkup(
 				[
                                         [
-					InlineKeyboardButton('❤ Donation Link', url='https://www.telegram.dog/greymatters_about')
+					InlineKeyboardButton('❤ Donation Link', url='https://t.me/ideal_bot_father')
 					],
 					[
-					InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/GreyMatter_Bots")
+					InlineKeyboardButton("Updates 𝙲𝚑𝚊𝚗𝚗𝚊𝚕", url="https://t.me/ideal_bot_father")
 					],
 					[
 					InlineKeyboardButton("Donation", callback_data="Help_msg"),
